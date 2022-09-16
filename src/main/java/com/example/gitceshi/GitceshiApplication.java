9c7538041d981c743efc7e145ca18ffb4264f5b4
Misteryun <liyunfiveyears@gmail.com>
2022-09-16 17:07:48 +0800

    222222

--- src/main/java/com/example/gitceshi/GitceshiApplication.java
+++ src/main/java/com/example/gitceshi/GitceshiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitceshiApplication {
 
     public static void main(String[] args) {
-        System.out.println("11111");
+        System.out.println("22222");
         SpringApplication.run(GitceshiApplication.class, args);
     }
 
